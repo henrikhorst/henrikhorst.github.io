dd520a9cd0879d9c8cbc874be2e2c78d9a212d4c
Henrik Horst <henrik.ho@gmx.de>
2024-01-12 05:55:52 +0100

    personal website initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-form').addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    // You can add more JavaScript to handle form submission
+
+    alert('Message sent!');
+});
